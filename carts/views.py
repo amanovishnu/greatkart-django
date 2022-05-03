028e776211235e0316af90bf1d551154578e4755
amanovishnu <geekymano@gmail.com>
2022-05-03 10:20:38 +0530

    replaced radio btn with dropdown for product variations

--- carts/views.py
+++ carts/views.py
@@ -14,6 +14,8 @@ def _cart_id(request): # private method
 
 
 def add_cart(request, product_id):
+    color = request.GET['color']
+    size = request.GET['size']
     product = Product.objects.get(id=product_id)
     try:
         cart = Cart.objects.get(cart_id=_cart_id(request)) # gets the cart using the cart_id in the session
