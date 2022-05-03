ce08132a51749e9ae2faf1c86c0c662658d62383
amanovishnu <geekymano@gmail.com>
2022-05-03 12:05:28 +0530

    indented as per PEP8

--- carts/admin.py
+++ carts/admin.py
@@ -4,9 +4,12 @@ from . import models
 class CartAdmin(admin.ModelAdmin):
     list_display = ['cart_id', 'date_added']
 
+
 admin.site.register(models.Cart, CartAdmin)
+
 
 class CartItemAdmin(admin.ModelAdmin):
     list_display = ['product', 'cart', 'quantity', 'is_active']
+
 
 admin.site.register(models.CartItem, CartItemAdmin)
