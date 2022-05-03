0ff35aadc23b4932598283a672584fd24f3b699d
amanovishnu <geekymano@gmail.com>
2022-05-03 12:06:42 +0530

    formatted as per PEP8

--- store/admin.py
+++ store/admin.py
@@ -1,19 +1,21 @@
 from django.contrib import admin
 from .models import Product, Variation
 
-# Register your models here.
+
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['product_name', 'slug', 'price', 'stock', 'is_available', 'category', 'created_date', 'modified_date']
     prepopulated_fields = {
         'slug':('product_name',)
     }
 
+
 admin.site.register(Product, ProductAdmin)
 
 
 class VariationAdmin(admin.ModelAdmin):
     list_display = ['product', 'variation_category', 'variation_value', 'created_date', 'is_active']
-    list_editable =  ['is_active']
     list_filter = ['product', 'variation_category', 'variation_value']
+    list_editable =  ['is_active']
+
 
 admin.site.register(Variation, VariationAdmin)
