9aa56220efd8982832ad913ea4ae81f5e58457e8
amanovishnu <geekymano@gmail.com>
2023-03-22 21:21:59 +0530

    info - user login & logout functionalities added

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages, auth
+from django.contrib.auth.decorators import login_required
 from .forms import RegistrationForm
 from .models import Account
 
@@ -22,13 +24,30 @@ def register(request):
                 )
             user.phone_number = phone_number
             user.save()
+            messages.success(request,'Registration Successful')
+            return redirect('register')
     else:
         form = RegistrationForm()
     data = {'form': form}
     return render(request, 'accounts/register.html', context=data)
 
 def login(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = auth.authenticate(email=email, password=password)
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'Login Successful. you\'re now logged in')
+            return redirect('home')
+        else:
+            messages.error(request, 'Invalid Login credentials. please check your details')
+            return redirect('login')
     return render(request, 'accounts/login.html')
 
+
+@login_required(login_url='login')
 def logout(request):
-    return render(request, 'accounts/logout.html')
+    auth.logout(request)
+    messages.success(request, 'You are Logged out Successfully')
+    return redirect('login')
