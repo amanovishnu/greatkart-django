b0f98526d7aded57fd52ee9d47c9c672f3a5c1ca
amanovishnu <geekymano@gmail.com>
2023-03-27 09:39:09 +0530

    fix - UI changes

--- greatkart/templatetags/custom_filters.py
+++ greatkart/templatetags/custom_filters.py
@@ -2,7 +2,7 @@ from django import template
 import math
 register = template.Library()
 
-@register.filter
+@register.filter(name='show_discounted_price')
 def show_discounted_price(sale_price, discount):
     final_amount = sale_price*(1+(discount/100))
     return math.floor(final_amount)
